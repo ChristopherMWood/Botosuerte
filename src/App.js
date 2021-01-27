53934e9524dfe6d561cbec1f759fddff3b0bd7b5
Christopher Wood <cmw2379@gmail.com>
2021-01-26 22:35:37 -0500

    Updated styles

--- src/App.js
+++ src/App.js
@@ -2,23 +2,38 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+function HomePage() {
+  return (
+    <h1>Test</h1>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="menuContainer">
+        <h1>Menu</h1>
+        <ul>
+          <li><a className="active" href="contact.asp">Chat Feed</a></li>
+          <li><a href="about.asp">Commands</a></li>
+          <li><a href="about.asp">Settings</a></li>
+        </ul>
+      </div>
+      <div className="contentContainer">
+        <h1>Features to write</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Settings page with credential inputs (pass protected)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Start chat button if settings are filled in
+        </p>
+        <p>
+          live chat view from IRC
+        </p>
+        <p>
+          toggle view for bot commands to turn on and off
+        </p>
+      </div>
     </div>
   )
 }
